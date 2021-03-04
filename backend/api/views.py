f6d6d8a199b25ee4269aa628a465e2c44220119e
damiankuchta <damkuch@gmail.com>
2021-03-04 19:21:32 +0000

    fixed bug where completed_date is updated only on is_completed change

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from rest_framework.response import Response
 from rest_framework import viewsets
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
@@ -25,9 +26,23 @@ class TaskViewSet(viewsets.ModelViewSet):
         return queryset
 
     def update(self, request, *args, **kwargs):
-        if request.data['is_completed']:
-            request.data['completed_date'] = date.today()
-        else:
-            request.data['completed_date'] = None
-        return super().update(request, *args, **kwargs)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        if request.data['is_completed'] != instance.is_completed:
+            if request.data['is_completed']:
+                request.data['completed_date'] = date.today()
+            else:
+                request.data['completed_date'] = None
+
+        return Response(serializer.data)
+
 
